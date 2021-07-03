e35a556d9a11095c8f03e982301fc5897b7ec2b6
cenk ayla <falconsaho105@hotmail.com>
2021-07-03 15:36:01 +0300

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r.HandleFunc("/api/task", middleware.GetAllTasks).Methods("GET")
 	r.HandleFunc("/api/task", middleware.Create).Methods("POST")
 	r.HandleFunc("/api/deletetask/{id}", middleware.Delete).Methods("DELETE")
-	r.HandleFunc("/api/deleteAllTask", middleware.DeleteAll).Methods("DELETE")
+	r.HandleFunc("/api/deletealltask", middleware.DeleteAll).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
